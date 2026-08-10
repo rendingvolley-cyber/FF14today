03d797bff35b3812add20381f76ddd094218f7a3
rendingvolley-cyber <rendingvolley@gmail.com>
2026-08-10 09:34:15 +0900

    Preserve retainer-first wrapper in combat flow

--- src/combat-job-wrapper.js
+++ src/combat-job-wrapper.js
@@ -1,4 +1,4 @@
-import app from "./retainer-market-wrapper.js";
+import app from "./retainer-first-wrapper.js";
 import { applyCombatJobFocus } from "./combat-job-focus.js";
 
 const OWNER_LODESTONE_ID = "3091607";
